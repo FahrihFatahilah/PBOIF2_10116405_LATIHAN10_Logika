6ac498bdd5601cd13f3975b57c18aa7951af3ad7
Fahrih Fatahilah <painchikusudo@gmail.com>
2020-10-20 12:34:13 +0800

    PBOIF2

---
+++ src/PBOIF2_10116405_Latihan10Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fahrih Fatahilah
+ * Nama  : Fahrih Fatahilah
+ * Kelas : IF-2 / PBO-IF2
+ * Nim   : 10116405
+ * 
+ */
+public class PBOIF2_10116405_Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
